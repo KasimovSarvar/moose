9242b21dde70a6c86842c6e93633919a9df7156c
sarvar <sarvaruzb01@gmail.com>
2024-12-06 18:12:44 +0500

    updated

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.getenv('DEBUG', 1))
+DEBUG = int(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = ['*']
 
